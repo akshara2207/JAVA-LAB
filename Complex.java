0124df185c96ed2fa57a9c4b2634af6f8aa0d200
akshara2207 <117832952+akshara2207@users.noreply.github.com>
2023-03-27 15:32:00 +0530

    Add files via upload

--- Complex.java
+++ Complex.java
@@ -1,43 +1,21 @@
-public class Complex {
-    private double real;
-    private double imaginary;
-    
-    // Constructor to initialize the complex number
-    public Complex(double real, double imaginary) {
-        this.real = real;
-        this.imaginary = imaginary;
-    }
-    
-    // Method to add two complex numbers
-    public Complex add(Complex other) {
-        double realSum = this.real + other.real;
-        double imaginarySum = this.imaginary + other.imaginary;
-        return new Complex(realSum, imaginarySum);
-    }
-    
-    // Method to print the complex number
-    public void print() {
-        if (imaginary < 0) {
-            System.out.println(real + " - " + (-imaginary) + "i");
-        } else {
-            System.out.println(real + " + " + imaginary + "i");
-        }
-    }
-    
-    public static void main(String[] args) {
-        // Create two complex numbers
-        Complex c1 = new Complex(2.0, 3.0);
-        Complex c2 = new Complex(4.0, -5.0);
-        
-        // Add the complex numbers
-        Complex sum = c1.add(c2);
-        
-        // Print the result
-        System.out.print("The sum of ");
-        c1.print();
-        System.out.print(" and ");
-        c2.print();
-        System.out.print(" is ");
-        sum.print();
-    }
+import java.util.Scanner;
+class Complex
+{
+public static void main(String arg[])
+{
+Scanner s=new Scanner (System.in);
+int a,b,c,d;
+System.out.println("enter 1st complex no");
+a=s.nextInt();
+b=s.nextInt();
+System.out.println("the no is:"+a+"+"+b+"i");
+System.out.println("enter 2nd complex no");
+c=s.nextInt();
+d=s.nextInt();
+System.out.println("the no is:"+c+"+"+d+"i");
+System.out.println("the sum is:"+(a+c)+"+"+(b+d)+"i");
 }
+}
+
+
+
